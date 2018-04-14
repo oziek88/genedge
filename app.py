14e2c9a9d243e04ed4e432f0834c9eb73988ca55
Aaron Newman <aaronnewman@AaronNMBP.local>
2018-04-14 15:09:52 -0700

    Get historical data for multiple days

--- app.py
+++ app.py
@@ -3,11 +3,12 @@ from flask import render_template
 from flask import request
 from flask import jsonify
 import requests
-import pandas as pd
+# import pandas as pd
 
 app = Flask(__name__)
 
 API_KEY='0fce4f7d72de5c934ab7dc9691c6447b'
+SECONDS_PER_DAY=86400
 
 @app.route("/")
 def homepage():
@@ -28,16 +29,19 @@ def homepage():
 def get_forecast():
     latitude = request.args.get('latitude')
     longitude = request.args.get('longitude')
-    response = jsonify(requests.get('https://api.darksky.net/forecast/{}/{},{}'.format(API_KEY, latitude, longitude)).json()['hourly'])
+    response = jsonify(requests.get('https://api.darksky.net/forecast/{}/{},{}'.format(API_KEY, latitude, longitude)).json()['hourly']['data'])
     return response
 
 @app.route("/weather_historical")
 def get_historical():
     latitude = request.args.get('latitude')
     longitude = request.args.get('longitude')
-    unix_time = request.args.get('unix_time')
-    response = jsonify(requests.get('https://api.darksky.net/forecast/{}/{},{},{}'.format(API_KEY, latitude, longitude, unix_time)).json()['hourly'])
-    return response
+    unix_time = int(request.args.get('unix_time'))
+    aggregate_response = []
+    for i in range(0, 8):
+        aggregate_response = aggregate_response + requests.get('https://api.darksky.net/forecast/{}/{},{},{}'.format(API_KEY, latitude, longitude, unix_time)).json()['hourly']['data']
+        unix_time = unix_time + SECONDS_PER_DAY
+    return jsonify(aggregate_response)
     
 if __name__ == "__main__":
     # data = pd.read_csv("All_Data_Hourly_8760_for_input.csv")
